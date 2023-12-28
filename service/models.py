2adc50df4769f3bd7636cb7b9cd2edee99149889
jesushova <jesushova@yahoo.com>
2023-12-28 03:48:14 -0500

    Updated feature and step definitions, and made changes in service and tests

--- service/models.py
+++ service/models.py
@@ -1,13 +1,12 @@
 # Copyright 2016, 2023 John Rofrano. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the 'License');
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
 # https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -34,26 +33,20 @@ from decimal import Decimal
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 logger = logging.getLogger("flask.app")
 
 # Create the SQLAlchemy object to be initialized later in init_db()
 db = SQLAlchemy()
 
-
 def init_db(app):
     """Initialize the SQLAlchemy app"""
     Product.init_db(app)
 
-
 class DataValidationError(Exception):
     """Used for an data validation errors when deserializing"""
 
-
 class Category(Enum):
     """Enumeration of valid Product Categories"""
-
     UNKNOWN = 0
     CLOTHS = 1
     FOOD = 2
@@ -61,155 +54,86 @@ class Category(Enum):
     AUTOMOTIVE = 4
     TOOLS = 5
 
-
 class Product(db.Model):
     """
     Class that represents a Product
-
-    This version uses a relational database for persistence which is hidden
-    from us by SQLAlchemy's object relational mappings (ORM)
     """
-
-    ##################################################
-    # Table Schema
-    ##################################################
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(250), nullable=False)
     price = db.Column(db.Numeric, nullable=False)
     available = db.Column(db.Boolean(), nullable=False, default=True)
-    category = db.Column(
-        db.Enum(Category), nullable=False, server_default=(Category.UNKNOWN.name)
-    )
-
-    ##################################################
-    # INSTANCE METHODS
-    ##################################################
+    category = db.Column(db.Enum(Category), nullable=False, server_default=(Category.UNKNOWN.name))
 
     def __repr__(self):
         return f"<Product {self.name} id=[{self.id}]>"
 
     def create(self):
-        """
-        Creates a Product to the database
-        """
         logger.info("Creating %s", self.name)
-        # id must be none to generate next primary key
-        self.id = None  # pylint: disable=invalid-name
+        self.id = None
         db.session.add(self)
         db.session.commit()
 
     def update(self):
-        """
-        Updates a Product to the database
-        """
         logger.info("Saving %s", self.name)
-        if not self.id:
-            raise DataValidationError("Update called with empty ID field")
+        if not self.id or not Product.query.get(self.id):
+            raise DataValidationError("Update called with invalid ID field")
         db.session.commit()
 
     def delete(self):
-        """Removes a Product from the data store"""
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self) -> dict:
-        """Serializes a Product into a dictionary"""
         return {
             "id": self.id,
             "name": self.name,
             "description": self.description,
             "price": str(self.price),
             "available": self.available,
-            "category": self.category.name  # convert enum to string
+            "category": self.category.name
         }
 
     def deserialize(self, data: dict):
-        """
-        Deserializes a Product from a dictionary
-        Args:
-            data (dict): A dictionary containing the Product data
-        """
         if not isinstance(data, dict):
             raise DataValidationError("Invalid data format for deserializing a Product")
-
         try:
             self.name = data["name"]
             self.description = data["description"]
             self.price = Decimal(data["price"])
             self.available = data["available"]
-            self.category = getattr(Category, data["category"])  # create enum from string
+            self.category = getattr(Category, data["category"])
         except KeyError as error:
             raise DataValidationError(f"Invalid product: missing {error.args[0]}")
         except (TypeError, ValueError) as error:
             raise DataValidationError(f"Invalid value for product: {error}")
-
         return self
-
-    ##################################################
-    # CLASS METHODS
-    ##################################################
 
     @classmethod
     def init_db(cls, app: Flask):
-        """Initializes the database session
-
-        :param app: the Flask app
-        :type data: Flask
-
-        """
         logger.info("Initializing database")
-        # This is where we initialize SQLAlchemy from the Flask app
         db.init_app(app)
         app.app_context().push()
-        db.create_all()  # make our sqlalchemy tables
+        db.create_all()
 
     @classmethod
     def all(cls) -> list:
-        """Returns all of the Products in the database"""
         logger.info("Processing all Products")
         return cls.query.all()
 
     @classmethod
     def find(cls, product_id: int):
-        """Finds a Product by it's ID
-
-        :param product_id: the id of the Product to find
-        :type product_id: int
-
-        :return: an instance with the product_id, or None if not found
-        :rtype: Product
-
-        """
         logger.info("Processing lookup for id %s ...", product_id)
         return cls.query.get(product_id)
 
     @classmethod
     def find_by_name(cls, name: str) -> list:
-        """Returns all Products with the given name
-
-        :param name: the name of the Products you want to match
-        :type name: str
-
-        :return: a collection of Products with that name
-        :rtype: list
-
-        """
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)
 
     @classmethod
     def find_by_price(cls, price: Decimal) -> list:
-        """Returns all Products with the given price
-
-        :param price: the price to search for
-        :type name: float
-
-        :return: a collection of Products with that price
-        :rtype: list
-
-        """
         logger.info("Processing price query for %s ...", price)
         price_value = price
         if isinstance(price, str):
@@ -218,33 +142,10 @@ class Product(db.Model):
 
     @classmethod
     def find_by_availability(cls, available: bool = True) -> list:
-        """Returns all Products by their availability
-
-        :param available: True for products that are available
-        :type available: str
-
-        :return: a collection of Products that are available
-        :rtype: list
-
-        """
         logger.info("Processing available query for %s ...", available)
         return cls.query.filter(cls.available == available)
 
     @classmethod
     def find_by_category(cls, category: Category = Category.UNKNOWN) -> list:
-        """Returns all Products by their Category
-
-        :param category: values are ['MALE', 'FEMALE', 'UNKNOWN']
-        :type available: enum
-
-        :return: a collection of Products that are available
-        :rtype: list
-
-        """
         logger.info("Processing category query for %s ...", category.name)
         return cls.query.filter(cls.category == category)
-
-
-
-
-
--- service/routes.py
+++ service/routes.py
@@ -98,36 +98,66 @@ def create_products():
 # L I S T   A L L   P R O D U C T S
 ######################################################################
 
-#
-# PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
-#
+@app.route("/products", methods=["GET"])
+def list_products():
+    """List Products"""
+    app.logger.info("Request to list Products...")
+    products = Product.all()
+    results = [product.serialize() for product in products]
+    return jsonify(results), status.HTTP_200_OK
 
 ######################################################################
 # R E A D   A   P R O D U C T
 ######################################################################
 
-#
-# PLACE YOUR CODE HERE TO READ A PRODUCT
-#
+@app.route("/products/<int:product_id>", methods=["GET"])
+def read_product(product_id):
+    """
+    Retrieve a single Product
+    This endpoint will return a Product based on its id
+    """
+    app.logger.info("Request for product with id: %s", product_id)
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+    return jsonify(product.serialize()), status.HTTP_200_OK
 
 ######################################################################
 # U P D A T E   A   P R O D U C T
 ######################################################################
 
-#
-# PLACE YOUR CODE TO UPDATE A PRODUCT HERE
-#
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_products(product_id):
+    """Update a Product"""
+    app.logger.info("Request to update Product with id: %s", product_id)
+    check_content_type("application/json")
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+    
+    data = request.get_json()
+    app.logger.info("Updating Product to: %s", data)
+    try:
+        product.deserialize(data)
+        product.id = product_id
+        product.update()
+    except DataValidationError as error:
+        abort(status.HTTP_400_BAD_REQUEST, str(error))
+    
+    return jsonify(product.serialize()), status.HTTP_200_OK
 
 ######################################################################
 # D E L E T E   A   P R O D U C T
 ######################################################################
 
-
-#
-# PLACE YOUR CODE TO DELETE A PRODUCT HERE
-#
-
-
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_products(product_id):
+    """Delete a Product"""
+    app.logger.info("Request to delete Product with id: %s", product_id)
+    product = Product.find(product_id)
+    if product:
+        product.delete()
+    return '', status.HTTP_204_NO_CONTENT
 
 ######################################################################
 # READ A PRODUCT
